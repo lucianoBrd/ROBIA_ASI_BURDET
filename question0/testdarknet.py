307ed4711c78cdc2ec11ab9a2e2c24ba6cd05ca2
Lucien BURDET <lucien.burdet@cpe.fr>
2021-06-09 09:46:56 +0200

    question 0

--- question0/testdarknet.py
+++ question0/testdarknet.py
@@ -51,70 +51,70 @@ while True:
 
 
 
+	if cnt%10 == 0:
+		# initialize our lists of detected bounding boxes, confidences, and
+		# class IDs, respectively
+		boxes = []
+		confidences = []
+		classIDs = []
 
-	# initialize our lists of detected bounding boxes, confidences, and
-	# class IDs, respectively
-	boxes = []
-	confidences = []
-	classIDs = []
+		# loop over each of the layer outputs
+		for output in layerOutputs:
+			# loop over each of the detections
+			for detection in output:
+				# extract the class ID and confidence (i.e., probability) of
+				# the current object detection
+				scores = detection[5:]
+				classID = np.argmax(scores)
+				confidence = scores[classID]
 
-	# loop over each of the layer outputs
-	for output in layerOutputs:
-		# loop over each of the detections
-		for detection in output:
-			# extract the class ID and confidence (i.e., probability) of
-			# the current object detection
-			scores = detection[5:]
-			classID = np.argmax(scores)
-			confidence = scores[classID]
+				# filter out weak predictions by ensuring the detected
+				# probability is greater than the minimum probability
+				if confidence > CONFIDENCE_THRESHOLD:
+					# scale the bounding box coordinates back relative to the
+					# size of the image, keeping in mind that YOLO actually
+					# returns the center (x, y)-coordinates of the bounding
+					# box followed by the boxes' width and height
+					box = detection[0:4] * np.array([W, H, W, H])
+					(centerX, centerY, width, height) = box.astype("int")
 
-			# filter out weak predictions by ensuring the detected
-			# probability is greater than the minimum probability
-			if confidence > CONFIDENCE_THRESHOLD:
-				# scale the bounding box coordinates back relative to the
-				# size of the image, keeping in mind that YOLO actually
-				# returns the center (x, y)-coordinates of the bounding
-				# box followed by the boxes' width and height
-				box = detection[0:4] * np.array([W, H, W, H])
-				(centerX, centerY, width, height) = box.astype("int")
+					# use the center (x, y)-coordinates to derive the top and
+					# and left corner of the bounding box
+					x = int(centerX - (width / 2))
+					y = int(centerY - (height / 2))
 
-				# use the center (x, y)-coordinates to derive the top and
-				# and left corner of the bounding box
-				x = int(centerX - (width / 2))
-				y = int(centerY - (height / 2))
+					# update our list of bounding box coordinates, confidences,
+					# and class IDs
+					boxes.append([x, y, int(width), int(height)])
+					confidences.append(float(confidence))
+					classIDs.append(classID)
 
-				# update our list of bounding box coordinates, confidences,
-				# and class IDs
-				boxes.append([x, y, int(width), int(height)])
-				confidences.append(float(confidence))
-				classIDs.append(classID)
+		# apply non-maxima suppression to suppress weak, overlapping bounding
+		# boxes
+		idxs = cv2.dnn.NMSBoxes(boxes, confidences, CONFIDENCE_THRESHOLD,
+			CONFIDENCE_THRESHOLD)
 
-	# apply non-maxima suppression to suppress weak, overlapping bounding
-	# boxes
-	idxs = cv2.dnn.NMSBoxes(boxes, confidences, CONFIDENCE_THRESHOLD,
-		CONFIDENCE_THRESHOLD)
+		# ensure at least one detection exists
+		if len(idxs) > 0:
+			# loop over the indexes we are keeping
+			for i in idxs.flatten():
+				# extract the bounding box coordinates
+				(x, y) = (boxes[i][0], boxes[i][1])
+				(w, h) = (boxes[i][2], boxes[i][3])
 
-	# ensure at least one detection exists
-	if len(idxs) > 0:
-		# loop over the indexes we are keeping
-		for i in idxs.flatten():
-			# extract the bounding box coordinates
-			(x, y) = (boxes[i][0], boxes[i][1])
-			(w, h) = (boxes[i][2], boxes[i][3])
+				color = [int(c) for c in COLORS[classIDs[i]]]
 
-			color = [int(c) for c in COLORS[classIDs[i]]]
+				cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+				text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+				cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+					0.5, color, 2)
 
-			cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-			text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
-			cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-				0.5, color, 2)
+		# show the output image
+		cv2.imshow("output", cv2.resize(image,(800, 600)))
 
-	# show the output image
-	cv2.imshow("output", cv2.resize(image,(800, 600)))
-
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord("q"):
-		break
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord("q"):
+			break
 
 
 # do a bit of cleanup
